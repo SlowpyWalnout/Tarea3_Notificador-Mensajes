a03afc74410e9ef0d6fde72d2a57dbc911220a44
José Quintana <76418657+SlowpyWalnout@users.noreply.github.com>
2023-10-26 18:49:15 -0600

    segundo commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hola')
+class Notificador{
+    // envia mensajes a las personas
+}
+class Personas{
+    constructor(nombre, edad, profesion){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.profesion = profesion;
+    }
+    
+}
+class Mensaje{
+    enviar(){
+        console.log('mensaje a enviar')
+    }
+}
+
